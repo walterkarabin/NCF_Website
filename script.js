f33027b4a362b32db9baaa2b77a7dee80963d25f
Walter Karabin <walter.karabin@gmail.com>
2022-12-30 16:43:13 -0500

    Update
    added services page, contact us page, navigation aid

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
-window.addEventListener('scroll', reveal);
-window.addEventListener('DOMContentLoaded', winLoaded);
-window.addEventListener('keypress', function(event){
-    const ele = document.querySelector("#section2");
-
-    const eleRect = ele.getBoundingClientRect();
-    const eleY = eleRect.top + window.scrollY;
-
-    // this.window.scrollTo({
-    //     top: eleY,
-    //     behaviour: "smooth"
-    // });
-
-    ele.scrollIntoView({behavior: 'smooth'});
-});
-document.getElementById("swipeUp").addEventListener("click", function() {
-    // Your code here
-    const ele = document.querySelector("#section2");
-    ele.scrollIntoView({behavior: 'smooth'});
-});
+// Get all the sections on the page
+var sections = document.querySelectorAll('section');
+var section;
 
 function reveal(){
     var reveals = document.querySelectorAll('.reveal');
 
     for (let index = 0; index < reveals.length; index++) {
         
-        var windowHeight = window.innerHeight;
+        var windowHeight = window.pageYOffset || document.documentElement.scrollTop;
         var revealTop = reveals[index].getBoundingClientRect().top;
         var revealPoint = 150;
 
-        if (revealTop < windowHeight) {
+        if (revealTop <= windowHeight) {
             // add them to the active class to make them show
             reveals[index].classList.add('active');
         }
@@ -47,4 +30,70 @@ function winLoaded() {
         
         onLoaded.classList.add('loaded');
     }
+    reveal();
+    checkScrollPosition();
+    console.log(section);
+    console.log(document.documentElement.scrollTop);
 }
+
+// Check Scroll Position
+
+
+// Set up a function to check the current scroll position
+function checkScrollPosition() {
+  // Get the current scroll position of the page
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  // Loop through each section on the page
+  for (var i = 0; i < sections.length; i++) {
+    // Get the top position of the current section
+    var top = sections[i].offsetTop;
+
+    // If the current scroll position is greater than the top position of the section,
+    // it means the user has scrolled past that section
+    if (scrollTop >= top) {
+        if (i+2<sections.length+1) {
+            var secNum = i + 2;
+            section = 'section'+secNum;
+            changeSwipeText(section);
+        }
+        else{
+            section = 'section'+1;
+            changeSwipeText(section);
+        }
+    }
+  }
+}
+
+// function to change the text of the swipe text
+function changeSwipeText(section) {
+    const currentSection = document.getElementById(section);
+    const swipeText = document.getElementById('swipeText');
+    const arrow = document.getElementById('arrow');
+    if(section == 'section1'){
+        swipeText.textContent = "Back to top";
+        arrow.style.transform = 'rotateZ(180deg)'
+    }else{
+        swipeText.textContent = currentSection.querySelector('h2').textContent;
+        arrow.style.transform = 'rotateZ(0deg)'
+    }
+}
+
+// function to scroll to specific section
+function scrollToSection() {
+    const element = document.getElementById(section);
+    element.scrollIntoView({behavior: 'smooth'});
+}
+
+
+
+// Listen for the scroll event on the window
+window.addEventListener('scroll', checkScrollPosition);
+
+// event listeners
+// On Scroll
+window.addEventListener('scroll', reveal);
+// On window loaded
+window.addEventListener('DOMContentLoaded', winLoaded);
+
+document.getElementById("swipeUp").addEventListener("click", scrollToSection);
